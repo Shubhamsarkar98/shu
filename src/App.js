4f99aa6a80403e1454d923bbffa2476cf3338f86
Shubham <shubhamsarkar9435@gmail.com>
2022-05-17 07:53:35 +0530

    first

--- src/App.js
+++ src/App.js
@@ -20,10 +20,12 @@ function App() {
         `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=6dbcf6f1a79ee3ee10411e147b6808d2`
       )
       .then((res) => {
+        console.log(res.data)
         setdata({
           description:res.data.weather[0].description,
           temp: res.data.main.temp,
           temp_max: res.data.main.temp_max,
+          temp_min: res.data.main.temp_min,
           humidity: res.data.main.humidity,
           sunrise: res.data.sys.sunrise,
           sunset:res.data.sys.sunset ,
